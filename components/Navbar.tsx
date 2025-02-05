caa0fd6628d57847ca6451681c252f37c8d9f09d
alim-99 <alim16097side@gmail.com>
2025-02-05 16:56:45 +0200

    navbar updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,15 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 const Navbar = () => {
   return (
     <header className='flex py-5'>
-      <Image 
-      src="/logo.svg" 
-      alt='logo'
-      width={50}
-      height={50} 
-      className='ml-3 mb-4'
-      />
+      <Link href="/">
+        <Image 
+          src="/logo.svg" 
+          alt='logo'
+          width={50}
+          height={50} 
+          className='ml-3 mb-4'
+        />
+      </Link>
       <nav className='flex-end justify-between flex-col ml-auto text-2xl dark:text-gray-400 text-slate-700'>
         <Link className='mr-2 dark:hover:text-gray-500 hover:text-slate-900' href="/">Home</Link>
         <Link className='mr-2 dark:hover:text-gray-500 hover:text-slate-900' href="/blog">Blog</Link>
