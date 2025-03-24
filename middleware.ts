f6b76c89f29bf27341ec8875155e28c88b15e035
alim-99 <alim16097side@gmail.com>
2025-03-25 00:19:30 +0200

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Protect all routes, including api and trpc
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    // Also protect API routes
+    '/(api|trpc)(.*)$'
   ],
 };
