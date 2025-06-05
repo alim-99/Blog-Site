36a9574e2d9a7dd336cd53aaa6fc0aeb40860a70
alim-99 <alim16097side@gmail.com>
2025-06-05 15:44:14 +0300

    navbar fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,26 +6,25 @@ import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <header className='flex py-5'>
+    <header className='flex items-center justify-between py-5 px-4'>
       <Link href="/">
         <Image 
           src="/logo.svg" 
           alt='logo'
           width={50}
           height={50} 
-          className='ml-3 mb-4'
         />
       </Link>
-      <nav className='flex-end justify-between flex-col ml-auto text-2xl dark:text-gray-400 text-slate-700'>
-        <Link className='mr-2 dark:hover:text-gray-500 hover:text-slate-900' href="/">Home</Link>
-        <Link className='mr-2 dark:hover:text-gray-500 hover:text-slate-900' href="/blog">Blog</Link>
-        <Link className='mr-5 dark:hover:text-gray-500 hover:text-slate-900' href="/posts">Posts</Link>
+      <nav className='flex items-center gap-6 text-2xl dark:text-gray-400 text-slate-700'>
+        <Link className='hover:text-slate-500' href="/">Home</Link>
+        <Link className='hover:text-slate-500' href="/blog">Blog</Link>
+        <Link className='hover:text-slate-500' href="/posts">Posts</Link>
         <ModeToggle />
-        <span className='relative mr-2 top-1'>
+        {/* <span className='relative mr-2 top-1'>
+        </span> */}
           <SignedIn>
             <UserButton />
           </SignedIn>
-        </span>
       </nav>
     </header>
   )
